71f066180dff0b8ca1da7c71d3b94a758e720b93
danielschoder <daniel@schoder.uk>
2024-08-17 13:12:44 +0100

    V10-59

---
+++ VoiceFlexTestUI/app.js
@@ -0,0 +1,5 @@
+"use strict";
+//Open in Terminal
+//concurrently "http-server -a localhost -p 8080"
+console.log('Hello world');
+//# sourceMappingURL=app.js.map
--- VoiceFlexTestUI/app.ts
+++ VoiceFlexTestUI/app.ts
@@ -1,1 +1,4 @@
+//Open in Terminal
+//concurrently "http-server -a localhost -p 8080"
+
 console.log('Hello world');
